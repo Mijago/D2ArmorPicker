6459fdb1a99dbb1628560e441ec09cb08b293557
Mijago <co.ding.mijago@googlemail.com>
2021-07-28 08:41:19 +0200

    Removed "0" from stat selection and set min width to 55px (from 64px)

--- src/app/components/authenticated/stat-minimum-selection/stat-minimum-selection.component.ts
+++ src/app/components/authenticated/stat-minimum-selection/stat-minimum-selection.component.ts
@@ -7,7 +7,7 @@ import {EventEmitter} from '@angular/core';
   styleUrls: ['./stat-minimum-selection.component.css']
 })
 export class StatMinimumSelectionComponent implements OnInit {
-  @Input() values: number[] = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+  @Input() values: number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   @Input() value: number = 10;
   @Input() maxPossibleValue: number = 100;
   @Output() onChange = new EventEmitter<number>();
