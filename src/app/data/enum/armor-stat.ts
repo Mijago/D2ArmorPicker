d8e49a56bc4c0fe68cd874f63cfc1a41595edd80
Mijago <co.ding.mijago@googlemail.com>
2021-08-06 12:08:19 +0200

    #22 Added ArmorStatNames

--- src/app/data/enum/armor-stat.ts
+++ src/app/data/enum/armor-stat.ts
@@ -1,3 +1,5 @@
+import {EnumDictionary} from "../types/EnumDictionary";
+
 export enum ArmorStat {
   Mobility,
   Resilience,
@@ -5,6 +7,15 @@ export enum ArmorStat {
   Discipline,
   Intellect,
   Strength
+}
+
+export const ArmorStatNames: EnumDictionary<ArmorStat, string> = {
+  [ArmorStat.Mobility]: "Mobility",
+  [ArmorStat.Resilience]: "Resilience",
+  [ArmorStat.Recovery]: "Recovery",
+  [ArmorStat.Discipline]: "Discipline",
+  [ArmorStat.Intellect]: "Intellect",
+  [ArmorStat.Strength]: "Strength"
 }
 
 
