ce733e4282f872c283c02fed1a1e7f99f7cf260c
Mijago <co.ding.mijago@googlemail.com>
2021-09-07 09:37:46 +0200

    #22 minor import cleanups

--- src/app/components/authenticated-v2/app-v2-core/app-v2-core.component.ts
+++ src/app/components/authenticated-v2/app-v2-core/app-v2-core.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Configuration} from "../../../data/configuration";
-import {MAXIMUM_STAT_MOD_AMOUNT} from "../../../data/constants";
-import {CharacterClass} from "../../../data/enum/character-Class";
-import {ArmorStat} from "../../../data/enum/armor-stat";
 import {ConfigurationService} from "../../../services/v2/configuration.service";
 import {StatusProviderService} from "../../../services/v2/status-provider.service";
 
--- src/app/components/authenticated-v2/settings/settings.component.ts
+++ src/app/components/authenticated-v2/settings/settings.component.ts
@@ -1,6 +1,4 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Configuration} from "../../../data/configuration";
-import {ConfigurationService} from "../../../services/v2/configuration.service";
 
 @Component({
   selector: 'app-settings',
@@ -9,7 +7,7 @@ import {ConfigurationService} from "../../../services/v2/configuration.service";
 })
 export class SettingsComponent implements OnInit {
 
-  constructor(private config: ConfigurationService) {
+  constructor() {
   }
 
   ngOnInit(): void {
