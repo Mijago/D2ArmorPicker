da72a0c5933e8f7378c16d75be0558b390349789
Mijago <co.ding.mijago@googlemail.com>
2022-01-03 13:22:07 +0100

    Fixed ability cooldowns for tier 8 and 7

--- src/app/data/cooldown_definitions.ts
+++ src/app/data/cooldown_definitions.ts
@@ -1,5 +1,5 @@
 export const SUPER_COOLDOWN_RATIO_PER_TIER = [1.44, 1.276, 1.14, 1.0, 0.94, 0.90, 0.86, 0.822, 0.798, 0.776, 0.76]
-export const ABILITY_COOLDOWN_RATIO_PER_TIER = [1.25, 1.14, 1.04, 1.0, 0.839, 0.72, 0.62, 0.59, 0.56, 0.45, 0.39]
+export const ABILITY_COOLDOWN_RATIO_PER_TIER = [1.25, 1.14, 1.04, 1.0, 0.839, 0.72, 0.62, 0.55, 0.50, 0.455, 0.39]
 
 export function calculateTierValueAbility(tier: number, baseValue: number) {
   return Math.round(baseValue * ABILITY_COOLDOWN_RATIO_PER_TIER[tier])
