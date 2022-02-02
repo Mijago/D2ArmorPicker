9f8b719421ed8a25a1a6b5fcb48040bd280df9d3
Mijago <co.ding.mijago@googlemail.com>
2022-02-02 11:39:27 +0100

    Minor cleanups

--- src/app/components/authenticated-v2/components/item-icon/item-icon.component.ts
+++ src/app/components/authenticated-v2/components/item-icon/item-icon.component.ts
@@ -1,11 +1,6 @@
 import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
-import {DatabaseService} from "../../../../services/database.service";
-import {liveQuery} from "dexie";
 import {IManifestArmor} from "../../../../data/types/IManifestArmor";
 import {ItemIconServiceService} from "../../../../services/item-icon-service.service";
-function timeout(ms:number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 @Component({
   selector: 'item-icon',
@@ -25,7 +20,6 @@ export class ItemIconComponent implements AfterViewInit {
   }
 
   async ngAfterViewInit() {
-    //this.item = await this.db.manifestArmor.where('hash').equals(this.itemHash).first();
     this.item = await this.iconService.getItemCached(this.itemHash)
   }
 
