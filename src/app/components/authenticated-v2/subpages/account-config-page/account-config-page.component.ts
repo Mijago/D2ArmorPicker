a2d93c517d03d7c7786b7ba11d35a0f7d0910146
Mijago <co.ding.mijago@googlemail.com>
2022-02-18 20:40:10 +0100

    Added the missing files for the acocunt config page

---
+++ src/app/components/authenticated-v2/subpages/account-config-page/account-config-page.component.ts
@@ -0,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {DatabaseService} from "../../../../services/database.service";
+import {InventoryService} from "../../../../services/inventory.service";
+import {AuthService} from "../../../../services/auth.service";
+
+@Component({
+  selector: 'app-account-config-page',
+  templateUrl: './account-config-page.component.html',
+  styleUrls: ['./account-config-page.component.css']
+})
+export class AccountConfigPageComponent implements OnInit {
+
+  constructor(private db: DatabaseService, public inv: InventoryService, private loginService: AuthService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  async downloadArmorInformation() {
+    const armor = await this.db.inventoryArmor.toArray();
+
+    const url = window.URL
+      .createObjectURL(new Blob([JSON.stringify(armor, null, 2)]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'd2ap_armor.json');
+    document.body.appendChild(link);
+    link.click();
+  }
+
+  async downloadManifestInformation() {
+    const data = await this.db.manifestArmor.toArray();
+
+    const url = window.URL
+      .createObjectURL(new Blob([JSON.stringify(data, null, 1)]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'd2ap_manifest.json');
+    document.body.appendChild(link);
+    link.click();
+  }
+
+  async resetDatabase() {
+    localStorage.removeItem("LastManifestUpdate");
+    localStorage.removeItem("last-manifest-revision");
+    localStorage.removeItem("last-manifest-db-name");
+
+    localStorage.removeItem("LastArmorUpdate");
+    localStorage.removeItem("last-armor-db-name");
+
+    await this.db.resetDatabase();
+    await this.inv.refreshAll(true, true);
+  }
+
+  async resetEverything() {
+    localStorage.clear()
+    await this.db.resetDatabase();
+    await this.loginService.logout();
+  }
+}
