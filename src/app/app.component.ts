18505c68f4260b0db0200ca18fa4077bd40c9ec7
Mijago <co.ding.mijago@googlemail.com>
2022-08-20 20:24:07 +0200

    Added bungie foundation header (but disabled it)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {environment} from "../environments/environment";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'D2ArmorPicker';
