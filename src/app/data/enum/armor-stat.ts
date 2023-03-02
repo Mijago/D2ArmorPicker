fff7aec7151a0b18302cbe21aede7f58be7b4521
Alexander Fr <coding.mijago@gmail.com>
2023-03-02 17:16:37 +0100

    fixed artifice cost

--- src/app/data/enum/armor-stat.ts
+++ src/app/data/enum/armor-stat.ts
@@ -39,22 +39,22 @@ export const STAT_MOD_VALUES: EnumDictionary<StatModifier, [ArmorStat, number, n
   [StatModifier.NONE]: [ArmorStat.Strength, 0, 0, 0],
   [StatModifier.MINOR_MOBILITY]: [ArmorStat.Mobility, 5, 1, 204137529],
   [StatModifier.MAJOR_MOBILITY]: [ArmorStat.Mobility, 10, 3, 3961599962],
-  [StatModifier.ARTIFICE_MOBILITY]: [ArmorStat.Mobility, 0, 3, 0],
+  [StatModifier.ARTIFICE_MOBILITY]: [ArmorStat.Mobility, 3, 0, 0],
   [StatModifier.MINOR_RESILIENCE]: [ArmorStat.Resilience, 5, 2, 3682186345],
   [StatModifier.MAJOR_RESILIENCE]: [ArmorStat.Resilience, 10, 4, 2850583378],
-  [StatModifier.ARTIFICE_RESILIENCE]: [ArmorStat.Resilience, 0, 3, 0],
+  [StatModifier.ARTIFICE_RESILIENCE]: [ArmorStat.Resilience, 3, 0, 0],
   [StatModifier.MINOR_RECOVERY]: [ArmorStat.Recovery, 5, 2, 555005975],
   [StatModifier.MAJOR_RECOVERY]: [ArmorStat.Recovery, 10, 4, 2645858828],
-  [StatModifier.ARTIFICE_RECOVERY]: [ArmorStat.Recovery, 0, 3, 0],
+  [StatModifier.ARTIFICE_RECOVERY]: [ArmorStat.Recovery, 3, 0, 0],
   [StatModifier.MINOR_DISCIPLINE]: [ArmorStat.Discipline, 5, 1, 2623485440],
   [StatModifier.MAJOR_DISCIPLINE]: [ArmorStat.Discipline, 10, 3, 4048838440],
-  [StatModifier.ARTIFICE_DISCIPLINE]: [ArmorStat.Discipline, 0, 3, 0],
+  [StatModifier.ARTIFICE_DISCIPLINE]: [ArmorStat.Discipline, 3, 0, 0],
   [StatModifier.MINOR_INTELLECT]: [ArmorStat.Intellect, 5, 2, 1227870362],
   [StatModifier.MAJOR_INTELLECT]: [ArmorStat.Intellect, 10, 4, 3355995799],
-  [StatModifier.ARTIFICE_INTELLECT]: [ArmorStat.Intellect, 0, 3, 0],
+  [StatModifier.ARTIFICE_INTELLECT]: [ArmorStat.Intellect, 3, 0, 0],
   [StatModifier.MINOR_STRENGTH]: [ArmorStat.Strength, 5, 1, 3699676109],
   [StatModifier.MAJOR_STRENGTH]: [ArmorStat.Strength, 10, 3, 3253038666],
-  [StatModifier.ARTIFICE_STRENGTH]: [ArmorStat.Strength, 0, 3, 0]
+  [StatModifier.ARTIFICE_STRENGTH]: [ArmorStat.Strength, 3, 0, 0]
 }
 
 export const ArmorStatNames: EnumDictionary<ArmorStat, string> = {
