061ae1d5bb3371d66ee7ffd3e45d4a160ef316bb
Mijago <co.ding.mijago@googlemail.com>
2023-03-04 23:33:59 +0100

    Fix for the 3x3 replacement

--- src/app/services/results-builder.worker.spec.ts
+++ src/app/services/results-builder.worker.spec.ts
@@ -5,7 +5,7 @@ import {ArmorPerkOrSlot, ArmorStat} from "../data/enum/armor-stat";
 import {BuildConfiguration} from "../data/buildConfiguration";
 
 
-function buildTestItem(slot: ArmorSlot, isExotic: boolean, stats: number[]): ItemCombination {
+function buildTestItem(slot: ArmorSlot, isExotic: boolean, stats: number[], perk: ArmorPerkOrSlot = ArmorPerkOrSlot.None): ItemCombination {
   return new ItemCombination([{
     name: "item_" + slot,
     armor2: true,
@@ -30,7 +30,7 @@ function buildTestItem(slot: ArmorSlot, isExotic: boolean, stats: number[]): Ite
     itemType: 0,
     itemSubType: 0,
     masterworked: true,
-    perk: ArmorPerkOrSlot.None,
+    perk: perk,
     mayBeBugged: false,
     rarity: TierType.Superior,
     rawData: undefined,
@@ -93,6 +93,98 @@ describe('Results Worker', () => {
     expect(result.stats[4]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Intellect].value * 10)
     expect(result.stats[5]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Strength].value * 10)
   });
+  it('should swap 3x artifice mods around to replace old mods', () => {
+    // this is an edge case in which the artifice mod, which initially will be applied to
+    // mobility, must be moved to Recovery. Otherwise, this set would not be possible.
+
+    const runtime = buildRuntime()
+
+    const mockItems: ItemCombination[] = [
+      buildTestItem(ArmorSlot.ArmorSlotHelmet, true, [6, 27, 3, 19, 7, 6]),
+      buildTestItem(ArmorSlot.ArmorSlotGauntlet, false, [2, 10, 21, 24, 2, 7], ArmorPerkOrSlot.SlotArtifice),
+      buildTestItem(ArmorSlot.ArmorSlotChest, false, [6, 2, 23, 28, 2, 2], ArmorPerkOrSlot.SlotArtifice),
+      buildTestItem(ArmorSlot.ArmorSlotLegs, false, [11, 12, 10, 21, 8, 2], ArmorPerkOrSlot.SlotArtifice),
+    ]
+
+    const config = new BuildConfiguration()
+    config.minimumStatTiers[ArmorStat.Mobility].value = 6
+    config.minimumStatTiers[ArmorStat.Resilience].value = 6
+    config.minimumStatTiers[ArmorStat.Recovery].value = 10
+    config.minimumStatTiers[ArmorStat.Discipline].value = 10
+    config.minimumStatTiers[ArmorStat.Intellect].value = 0
+    config.minimumStatTiers[ArmorStat.Strength].value = 0
+
+    let result = handlePermutation(
+      runtime,
+      config, // todo config
+      mockItems[0],
+      mockItems[1],
+      mockItems[2],
+      mockItems[3],
+      [0, 0, 0, 0, 0, 0], // constant bonus
+      [5, 5, 5, 5, 5], // availableModCost
+      false, // doNotOutput
+      true // hasArtificeClassItem
+    )
+    expect(result).toBeDefined()
+    expect(result.stats[0]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Mobility].value * 10)
+    expect(result.stats[1]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Resilience].value * 10)
+    expect(result.stats[2]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Recovery].value * 10)
+    expect(result.stats[3]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Discipline].value * 10)
+    expect(result.stats[4]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Intellect].value * 10)
+    expect(result.stats[5]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Strength].value * 10)
+  });
+  it('should swap 3x artifice mods around to replace old mods v2', () => {
+    // this is an edge case in which the artifice mod, which initially will be applied to
+    // mobility, must be moved to Recovery. Otherwise, this set would not be possible.
+
+    const runtime = buildRuntime()
+
+    const mockItems: ItemCombination[] = [
+      buildTestItem(ArmorSlot.ArmorSlotHelmet, false, [13, 16, 2, 24, 2, 7], ArmorPerkOrSlot.SlotArtifice),
+      buildTestItem(ArmorSlot.ArmorSlotGauntlet, false, [26, 6, 2, 26, 2, 2], ArmorPerkOrSlot.SlotArtifice),
+      buildTestItem(ArmorSlot.ArmorSlotChest, true, [6, 24, 2, 17, 7, 7]),
+      buildTestItem(ArmorSlot.ArmorSlotLegs, false, [22, 9, 2, 24, 2, 6], ArmorPerkOrSlot.SlotArtifice),
+    ]
+
+    const config = new BuildConfiguration()
+    config.minimumStatTiers[ArmorStat.Mobility].value = 9
+    config.minimumStatTiers[ArmorStat.Resilience].value = 10
+    config.minimumStatTiers[ArmorStat.Recovery].value = 0
+    config.minimumStatTiers[ArmorStat.Discipline].value = 10
+    config.minimumStatTiers[ArmorStat.Intellect].value = 0
+    config.minimumStatTiers[ArmorStat.Strength].value = 0
+
+    const constantBonus = [-10, 0, 10, 0, 0, -10];
+    let result = handlePermutation(
+      runtime,
+      config, // todo config
+      mockItems[0],
+      mockItems[1],
+      mockItems[2],
+      mockItems[3],
+      constantBonus, // constant bonus
+      [5, 5, 5, 5, 5], // availableModCost
+      false, // doNotOutput
+      true // hasArtificeClassItem
+    )
+    expect(result).toBeDefined()
+    console.log(result)
+    expect(result.stats[0]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Mobility].value * 10)
+    expect(result.stats[1]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Resilience].value * 10)
+    expect(result.stats[2]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Recovery].value * 10)
+    expect(result.stats[3]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Discipline].value * 10)
+    expect(result.stats[4]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Intellect].value * 10)
+    expect(result.stats[5]).toBeGreaterThanOrEqual(config.minimumStatTiers[ArmorStat.Strength].value * 10)
+
+
+    for (let n = 0; n < 6; n++) {
+      const minor = 1 * result.mods.filter((mod: number) => Math.floor(mod / 3) == n && mod % 3 == 1).length
+      const major = 1 * result.mods.filter((mod: number) => Math.floor(mod / 3) == n && mod % 3 == 2).length
+      const artif = 1 * result.artifice.filter((mod: number) => Math.floor(mod / 3) - 1 == n && mod % 3 == 0).length
+      expect(result.stats[n]).toEqual(result.statsNoMods[n] + 5 * minor + 10 * major + 3 * artif + constantBonus[n])
+    }
+  });
 
 
   it('should be able to keep plain zero-waste builds', () => {
@@ -147,7 +239,7 @@ describe('Results Worker', () => {
     mockItems[0].items[0].recovery -= 5 // minor mod
     mockItems[0].items[0].discipline -= 5 // minor mod
     mockItems[0].items[0].intellect -= 5 // minor mod
-    mockItems[0].items[0].strength -= 5 -3 // minor mod + artifice mod
+    mockItems[0].items[0].strength -= 5 - 3 // minor mod + artifice mod
 
 
     const config = new BuildConfiguration()
