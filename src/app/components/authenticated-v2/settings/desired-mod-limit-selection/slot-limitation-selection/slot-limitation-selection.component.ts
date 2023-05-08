5ef7f822eddbb0c8129c752f04fabd53fb44b777
Mijago <co.ding.mijago@googlemail.com>
2023-05-08 16:22:36 +0200

    fix: small linting error

--- src/app/components/authenticated-v2/settings/desired-mod-limit-selection/slot-limitation-selection/slot-limitation-selection.component.ts
+++ src/app/components/authenticated-v2/settings/desired-mod-limit-selection/slot-limitation-selection/slot-limitation-selection.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
 import { MAXIMUM_STAT_MOD_AMOUNT } from "../../../../../data/constants";
 import { ArmorSlot } from "../../../../../data/enum/armor-slot";
 import { ConfigurationService } from "../../../../../services/configuration.service";
@@ -15,7 +23,7 @@ import { environment } from "../../../../../../environments/environment";
   templateUrl: "./slot-limitation-selection.component.html",
   styleUrls: ["./slot-limitation-selection.component.scss"],
 })
-export class SlotLimitationSelectionComponent implements OnInit, OnDestroy {
+export class SlotLimitationSelectionComponent implements OnInit, OnDestroy, AfterViewInit {
   readonly featureDisabled = !environment.featureFlags.enableModslotLimitation;
   readonly ArmorSlot = ArmorSlot;
   readonly ArmorPerkOrSlotNames = ArmorPerkOrSlotNames;
