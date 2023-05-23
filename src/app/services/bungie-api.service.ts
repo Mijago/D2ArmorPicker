9dffb7e987fbc64d64ae558d3743ce9d7d6c9075
Mijago <co.ding.mijago@googlemail.com>
2023-05-23 23:37:55 +0200

    fix: updated artifice armor id

--- src/app/services/bungie-api.service.ts
+++ src/app/services/bungie-api.service.ts
@@ -454,7 +454,9 @@ export class BungieApiService {
       return ArmorPerkOrSlot.GuardianGamesClassItem;
 
     const scks = v.sockets?.socketEntries;
-    if ((scks?.filter((d) => d.reusablePlugSetHash == 1259) || []).length > 0)
+    if ((scks?.filter((d) => d.reusablePlugSetHash == 1280) || []).length > 0)
+      return ArmorPerkOrSlot.SlotArtifice;
+    if ((scks?.filter((d) => d.singleInitialItemHash == 3727270518) || []).length > 0)
       return ArmorPerkOrSlot.SlotArtifice;
 
     if ((scks?.filter((d) => d.singleInitialItemHash == 4144354978) || []).length > 0)
