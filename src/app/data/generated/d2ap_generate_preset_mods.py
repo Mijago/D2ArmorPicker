1007aa8ae81d7a71a803aaf0fc25add11eea7688
Mijago <co.ding.mijago@googlemail.com>
2023-06-04 14:38:32 +0200

    docs: added AGPL-3.0 header to all files

--- src/app/data/generated/d2ap_generate_preset_mods.py
+++ src/app/data/generated/d2ap_generate_preset_mods.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2023 D2ArmorPicker by Mijago.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 A python function that takes a number and returns every possible combination of the factors 3, 5 and 10 which can lead to this number.
 It also returns the combination if it "just barely" exceeds N.
