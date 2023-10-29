62ed9d638a2c87f6c9e77b68eac24c861db4a19b
Mijago <coding.mijago@gmail.com>
2023-10-29 11:24:55 +0000

    feat: updated angular material 14->15

--- src/app/modules/common-material/common-material.module.ts
+++ src/app/modules/common-material/common-material.module.ts
@@ -43,7 +43,6 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { MatChipsModule } from "@angular/material/chips";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [],
@@ -75,10 +74,8 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     MatSidenavModule,
     ReactiveFormsModule,
     FormsModule,
-    FlexLayoutModule,
   ],
   exports: [
-    FlexLayoutModule,
     ReactiveFormsModule,
     FormsModule,
     MatButtonModule,
